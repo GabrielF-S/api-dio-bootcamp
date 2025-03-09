3e3082b4ca64f8ef0b030c427dc584531547ffc1
GabrielF-S <gabrielboni2008@hotmail.com>
2025-03-09 14:46:32 -0300

    fix: CORS

--- src/main/java/br/com/gabriel/api_dio_bootcamp/controller/UserController.java
+++ src/main/java/br/com/gabriel/api_dio_bootcamp/controller/UserController.java
@@ -2,13 +2,15 @@ package br.com.gabriel.api_dio_bootcamp.controller;
 
 import br.com.gabriel.api_dio_bootcamp.domain.model.User;
 import br.com.gabriel.api_dio_bootcamp.service.UserService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default server URL")})
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
